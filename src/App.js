40f73ef2e9119238da919c1c5fb9400ffeb7045f
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-09-05 02:08:33 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Search App</h1>
+            <p>This is a simple search app</p>
+            <SearchForm />
+          </Grid>
+        </Jumbotron>
+        <Results/>
+      </div>
+    );
+  }
+}
+
+export default App;
